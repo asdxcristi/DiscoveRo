691a223b9b239cc1e26b38e44c5b45bfa87b830a
biancatenisas <oana.bouleanu@stud.acs.upb.ro>
2018-04-26 12:18:11 +0300

    Added UserModel

--- app/models/UserModel.js
+++ app/models/UserModel.js
@@ -1,1 +1,23 @@
-//TODO: create the User schema for mongoose
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+	name:{
+		type: String,
+		required: true
+	},
+	email:{
+		type: String,
+		required: true
+	},
+	password:{
+		type: String,
+		required: true
+	},
+	friend_list: [String],
+	profile_pic: Buffer,
+	travel_history: [[]],
+	current_journey: [String]
+});
+
+const User = module.exports =
+mongoose.model('UserModel', UserSchema);
